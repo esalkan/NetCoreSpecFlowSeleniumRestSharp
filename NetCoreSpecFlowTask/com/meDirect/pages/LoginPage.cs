21f412958562de575e1f35c3053f39db3eb1ebb8
Eyüp Sabri ALKAN <esalkan@icloud.com>
2022-05-20 20:53:33 +0300

    Login Page UI Element locators and methods

--- NetCoreSpecFlowTask/com/meDirect/pages/LoginPage.cs
+++ NetCoreSpecFlowTask/com/meDirect/pages/LoginPage.cs
@@ -13,25 +13,29 @@ namespace NetCoreSpecFlowTask.com.meDirect.pages
             WebDriver = webDriver;
         }
         */
+        
         // UI Web Elements Locators
         public IWebElement inputUserName => Driver.getDriver().FindElement(By.XPath("//input[@id='user-name']"));
         public IWebElement inputPassword => Driver.getDriver().FindElement(By.XPath("//input[@id='password']"));
         public IWebElement loginButton => Driver.getDriver().FindElement(By.XPath("//input[@id='login-button']"));
+        public IWebElement wrongCredentialsErrorMessage => Driver.getDriver().FindElement(By.XPath("//h3[@data-test='error']"));
         
-        // Filling the login form with given credentials
-        public void fillTheLoginForm(string userName, string userPassword)
+        // Filling the login form with given data
+        public void Login(string userName, string userPassword)
         {
             inputUserName.SendKeys(userName);
             inputPassword.SendKeys(userPassword);
         } 
         
-        // Click method to the login button
-        public void clickOnLoginBtn()
+        // Login button
+        public void ClickOnLoginBtn()
         {
             loginButton.Click();
         }
 
-
-
+        public string GetWrongCredentialErrorMessage()
+        {
+            return wrongCredentialsErrorMessage.Text;
+        }
     }
 }
